a5b8dc7c7e8e571bdcf9b6d2a4ad6b13801ebb1c
Reficul <xuzhenglun@gmail.com>
2016-06-23 16:18:21 +0800

    add userinfo

--- bitbucket-v2/user.go
+++ bitbucket-v2/user.go
@@ -1,13 +1,27 @@
 package bitbucket_v2
 
+import "time"
+
 type User struct {
-	Username    string `json:"username"`
-	DisplayName string `json:"dispaly_name"`
-	UUID        string `json:"uuid"`
-	Type        string `json:"type"`
+	Username    string    `json:"username"`
+	Website     string    `json:"website"`
+	DisplayName string    `json:"dispaly_name"`
+	UUID        string    `json:"uuid"`
+	Type        string    `json:"type"`
+	Created     time.Time `json:"created_on"`
 }
 
 type AutherType struct {
 	User
 	Raw string `json:"raw"`
+}
+
+func (c *Client) Userinfo() (*User, error) {
+	user := User{}
+
+	if err := c.do("GET", "/user", nil, nil, "", &user); err != nil {
+		return nil, err
+	}
+
+	return &user, nil
 }
